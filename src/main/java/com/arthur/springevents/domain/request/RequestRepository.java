4bbd67087bd7be51536aa2ee87388e92c34815fa
Turao <lenz.arthur@gmail.com>
2021-01-24 02:20:06 -0300

    Add @Repository annotation to repositories

--- src/main/java/com/arthur/springevents/domain/request/RequestRepository.java
+++ src/main/java/com/arthur/springevents/domain/request/RequestRepository.java
@@ -3,7 +3,9 @@ package com.arthur.springevents.domain.request;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestRepository extends CrudRepository<Request, UUID> {
     List<Request> findAll();
 }
--- src/main/java/com/arthur/springevents/domain/translation/TranslationRepository.java
+++ src/main/java/com/arthur/springevents/domain/translation/TranslationRepository.java
@@ -2,5 +2,7 @@ package com.arthur.springevents.domain.translation;
 
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TranslationRepository extends CrudRepository<Translation, UUID> {}
